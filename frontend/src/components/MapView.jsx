3efeec49d94574f70fe962cf50b43d46497e863f
Jclora0313 <jclora0313@gmail.com>
2026-07-14 23:55:23 +0200

    fix chat backend favicon and leaflet markers

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -2,9 +2,21 @@ import {
   MapContainer,
   TileLayer,
   Marker,
-  Popup,
   Polyline,
 } from "react-leaflet";
+
+import L from "leaflet";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 function MapView({
   pickupLat,
